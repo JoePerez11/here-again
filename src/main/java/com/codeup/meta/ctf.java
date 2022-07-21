268507d9a2077e66ff5d4f693edbd86ef6519380
JoePerez11 <perez.joseph.k@gmail.com>
2022-07-21 15:04:21 -0500

    trying to deploy

--- src/main/java/com/codeup/meta/ctf.java
+++ src/main/java/com/codeup/meta/ctf.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MetaApplication {
+public class ctf {
 
     public static void main(String[] args) {
-        SpringApplication.run(MetaApplication.class, args);
+        SpringApplication.run(ctf.class, args);
     }
 
 }
