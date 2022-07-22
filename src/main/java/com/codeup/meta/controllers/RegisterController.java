ba9c58508afab1ce4c943be1668503d1fdab45d5
JoePerez11 <perez.joseph.k@gmail.com>
2022-07-22 11:05:05 -0500

    commiting current changes while troubleshooting deployment

--- src/main/java/com/codeup/meta/controllers/RegisterController.java
+++ src/main/java/com/codeup/meta/controllers/RegisterController.java
@@ -1,2 +1,15 @@
-package com.codeup.meta.controllers;public class RegisterController {
-}
+package com.codeup.meta.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+
+        @GetMapping(path = "/register")
+
+        public String login() {
+            return "register";
+        }
+    }
+
